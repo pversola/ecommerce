6651c4ce630112178dd91ef9db7440cf06caa062
Patrick Versola <pversola@gmail.com>
2019-09-25 23:42:57 +0700

    update our shop-page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 
 const HatsPage = () => (
   <div>
@@ -17,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
