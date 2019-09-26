b31d3f4dd3cd2d0c058fb1e259e818e31eb4f77d
Administrator <admin@example.com>
2019-09-26 19:35:03 +0700

    update sign-in component

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import './sign-up.styles.scss'
+
+class SignUp extends Component{
+    constructor() {
+        super()
+    }
+
+    render() {
+        return(
+            <div>SIGN UP</div>
+        )
+    }
+}
+
+export default SignUp
